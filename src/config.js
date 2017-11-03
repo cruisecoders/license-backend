e93eb433aa634f8131560a52b85df19d0cac5d2b
Archit Gupta <architgupta471@gmail.com>
2017-11-03 17:22:06 +0530

    Changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     Host: 'http://192.168.1.77:3000/',
-    mongoDB: 'mongodb://localhost/license',
+    mongoDB: (process.env.PROD)?'mongodb://example:example@ds245615.mlab.com:45615/license':'mongodb://localhost/license',
 };
